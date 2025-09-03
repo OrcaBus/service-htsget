a502c972bb74df4f713884bbd428bf86b9c84754
mmalenic <mmalenic1@gmail.com>
2025-09-03 17:02:19 +1000

    feat: add filemanager cross account buckets to htsget

--- infrastructure/stage/config.ts
+++ infrastructure/stage/config.ts
@@ -4,6 +4,7 @@ import { vpcProps } from './constants';
 import {
   FILE_MANAGER_BUCKETS,
   FILE_MANAGER_CACHE_BUCKETS,
+  FILE_MANAGER_CROSS_ACCOUNT_BUCKETS,
   FILE_MANAGER_INGEST_ROLE,
 } from '@orcabus/platform-cdk-constructs/shared-config/file-manager';
 import { StageName } from '@orcabus/platform-cdk-constructs/shared-config/accounts';
@@ -16,7 +17,11 @@ export const getHtsgetProps = (stage: StageName): HtsgetStackConfig => {
       apiName: 'Htsget',
       customDomainNamePrefix: 'htsget-file',
     },
-    buckets: [...FILE_MANAGER_BUCKETS[stage], ...FILE_MANAGER_CACHE_BUCKETS[stage]],
+    buckets: [
+      ...FILE_MANAGER_BUCKETS[stage],
+      ...FILE_MANAGER_CACHE_BUCKETS[stage],
+      ...FILE_MANAGER_CROSS_ACCOUNT_BUCKETS,
+    ],
     roleName: FILE_MANAGER_INGEST_ROLE,
     apiGatewayAuthProps: {
       ...getDefaultApiGatewayConfiguration(stage),
